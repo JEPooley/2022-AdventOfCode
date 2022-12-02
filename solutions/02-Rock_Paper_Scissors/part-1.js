ba85ca6e658395cbfb617955ea1b236d1cae47f7
Josh Pooley <joshpooley93@gmail.com>
2022-12-02 20:16:22 +0000

    day2: simplify lookup objects

--- solutions/02-Rock_Paper_Scissors/part-1.js
+++ solutions/02-Rock_Paper_Scissors/part-1.js
@@ -2,31 +2,16 @@
 
 const fs = require("fs");
 
-const shape = {
-  A: 1, // Rock
-  B: 2, // Paper
-  C: 3, // Scissors
-  X: 1, // Rock
-  Y: 2, // Paper
-  Z: 3, // Scissors
-};
-
-const outcome = {
-  AZ: 0, // Loss
-  BX: 0, // Loss
-  CY: 0, // Loss
-  AX: 3, // Draw
-  BY: 3, // Draw
-  CZ: 3, // Draw
-  AY: 6, // Win
-  BZ: 6, // Win
-  CX: 6, // Win
-};
-
-const getRoundScore = (round) => {
-  const outcomeScore = outcome[round];
-  const shapeScore = shape[round[1]];
-  return outcomeScore + shapeScore;
+const score = {
+  AZ: 3, // Loss
+  BX: 1, // Loss
+  CY: 2, // Loss
+  AX: 4, // Draw
+  BY: 5, // Draw
+  CZ: 6, // Draw
+  AY: 8, // Win
+  BZ: 9, // Win
+  CX: 7, // Win
 };
 
 const strategyGuide = fs.readFileSync("strategy_guide.txt", "utf8");
@@ -34,7 +19,7 @@ const strategyGuide = fs.readFileSync("strategy_guide.txt", "utf8");
 const rounds = strategyGuide.replaceAll(" ", "").split("\n");
 
 const totalScore = rounds
-  .map((round) => getRoundScore(round))
+  .map((round) => score[round])
   .reduce((a, b) => a + b);
 
 console.log("Total Score: ", totalScore);
--- solutions/02-Rock_Paper_Scissors/part-2.js
+++ solutions/02-Rock_Paper_Scissors/part-2.js
@@ -2,28 +2,16 @@
 
 const fs = require("fs");
 
-const outcome = {
-  X: 0, // Lose
-  Y: 3, // Draw
-  Z: 6, // Win
-};
-
-const shape = {
+const score = {
   BX: 1, // Rock
-  AY: 1, // Rock
-  CZ: 1, // Rock
+  AY: 4, // Rock
+  CZ: 7, // Rock
   CX: 2, // Paper
-  BY: 2, // Paper
-  AZ: 2, // Paper
+  BY: 5, // Paper
+  AZ: 8, // Paper
   AX: 3, // Scissors
-  CY: 3, // Scissors
-  BZ: 3, // Scissors
-};
-
-const getOutcome = (round) => {
-  const shapeScore = shape[round];
-  const outcomeScore = outcome[round[1]];
-  return outcomeScore + shapeScore;
+  CY: 6, // Scissors
+  BZ: 9, // Scissors
 };
 
 const strategyGuide = fs.readFileSync("strategy_guide.txt", "utf8");
@@ -31,7 +19,7 @@ const strategyGuide = fs.readFileSync("strategy_guide.txt", "utf8");
 const rounds = strategyGuide.replaceAll(" ", "").split("\n");
 
 const totalScore = rounds
-  .map((round) => getOutcome(round))
+  .map((round) => score[round])
   .reduce((a, b) => a + b);
 
 console.log("Total Score: ", totalScore);
