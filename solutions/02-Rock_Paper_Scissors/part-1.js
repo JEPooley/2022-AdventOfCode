62bf8b2008bccaf1619402750c3e9e228407d97a
Josh Pooley <joshpooley93@gmail.com>
2022-12-02 20:17:53 +0000

    day2: update comments

--- solutions/02-Rock_Paper_Scissors/part-1.js
+++ solutions/02-Rock_Paper_Scissors/part-1.js
@@ -3,23 +3,21 @@
 const fs = require("fs");
 
 const score = {
-  AZ: 3, // Loss
-  BX: 1, // Loss
-  CY: 2, // Loss
-  AX: 4, // Draw
-  BY: 5, // Draw
-  CZ: 6, // Draw
-  AY: 8, // Win
-  BZ: 9, // Win
-  CX: 7, // Win
+  AZ: 3,
+  BX: 1,
+  CY: 2,
+  AX: 4,
+  BY: 5,
+  CZ: 6,
+  AY: 8,
+  BZ: 9,
+  CX: 7,
 };
 
 const strategyGuide = fs.readFileSync("strategy_guide.txt", "utf8");
 
 const rounds = strategyGuide.replaceAll(" ", "").split("\n");
 
-const totalScore = rounds
-  .map((round) => score[round])
-  .reduce((a, b) => a + b);
+const totalScore = rounds.map((round) => score[round]).reduce((a, b) => a + b);
 
 console.log("Total Score: ", totalScore);
--- solutions/02-Rock_Paper_Scissors/part-2.js
+++ solutions/02-Rock_Paper_Scissors/part-2.js
@@ -3,23 +3,21 @@
 const fs = require("fs");
 
 const score = {
-  BX: 1, // Rock
-  AY: 4, // Rock
-  CZ: 7, // Rock
-  CX: 2, // Paper
-  BY: 5, // Paper
-  AZ: 8, // Paper
-  AX: 3, // Scissors
-  CY: 6, // Scissors
-  BZ: 9, // Scissors
+  BX: 1,
+  AY: 4,
+  CZ: 7,
+  CX: 2,
+  BY: 5,
+  AZ: 8,
+  AX: 3,
+  CY: 6,
+  BZ: 9,
 };
 
 const strategyGuide = fs.readFileSync("strategy_guide.txt", "utf8");
 
 const rounds = strategyGuide.replaceAll(" ", "").split("\n");
 
-const totalScore = rounds
-  .map((round) => score[round])
-  .reduce((a, b) => a + b);
+const totalScore = rounds.map((round) => score[round]).reduce((a, b) => a + b);
 
 console.log("Total Score: ", totalScore);
